ec6fc4b73991e2d88367a86b3f21a87da5d1d2a3
anhartt <alexharttree@gmail.com>
2024-07-19 00:24:50 -0400

    Work

--- src/components/Anubis.js
+++ src/components/Anubis.js
@@ -4,7 +4,7 @@ import React from 'react';
 function Anubis() {
   return (
     <div>
-      <h1>Welcome to the New Page</h1>
+      <h1>Welcome to the Anubis Page</h1>
       <p>This is the content of the new page.</p>
     </div>
   );
